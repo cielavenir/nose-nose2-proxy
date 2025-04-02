5cf0fa5ad7e8df3df39ede20cd381d8cb085cbde
cielavenir <cielartisan@gmail.com>
2025-04-02 20:45:36 +0900

    module

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## enum34-aenum-proxy
+## nose-nose2-proxy
 
-Overrides [enum34](https://pypi.org/project/enum34/) installation to use [aenum](https://pypi.org/project/aenum/)
+Overrides [nose](https://pypi.org/project/nose/) installation to use [nose2](https://pypi.org/project/nose2/)
 
-install: pip install git+https://github.com/cielavenir/enum34-aenum-proxy.git
+install: pip install git+https://github.com/cielavenir/nose-nose2-proxy.git
 
-please don't upload to repos like pypi to respect original enum34 author.
+please don't upload to repos like pypi to respect original nose author.
--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='enum34',
-    version="2.0.0",
+    name='nose',
+    version="1.0.0",
     author='cielavenir',
     author_email='cielartisan@gmail.com',
-    description='fake enum34 redirecting to aenum',
+    description='fake nose redirecting to nose2',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/cielavenir/enum34-aenum-proxy',
-    keywords='enum enum34 aenum',
+    url='https://github.com/cielavenir/nose-nose2-proxy',
+    keywords='nose nose2',
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2',
@@ -23,8 +23,8 @@ setuptools.setup(
         'Topic :: Software Development',
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=2.7, <3.4",
+    # python_requires=">=2.7",
     install_requires=[
-        'aenum',
+        'nose2',
     ],
 )
